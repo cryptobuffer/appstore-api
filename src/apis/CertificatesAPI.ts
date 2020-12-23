56fe59dd84de5fa1cf78255ab54395d2566c8773
cryptobuffer <cryptobuffer@core.local>
2020-12-23 11:42:48 +0800

    feat: add certificate create method

--- src/apis/CertificatesAPI.ts
+++ src/apis/CertificatesAPI.ts
@@ -1,12 +1,31 @@
 import BaseAPI from './BaseAPI';
 
-declare type CertificateType = 'DEVELOPMENT' | 'DISTRIBUTION';
+declare type CertificateType = 'IOS_DEVELOPMENT' | 'IOS_DISTRIBUTION' | 'MAC_APP_DISTRIBUTION' | 'MAC_INSTALLER_DISTRIBUTION' | 'MAC_APP_DEVELOPMENT' | 'DEVELOPER_ID_KEXT' | 'DEVELOPER_ID_APPLICATION' | 'DEVELOPMENT' | 'DISTRIBUTION';
 
 export default class CertificatesAPI extends BaseAPI {
   public async list(params = {}): Promise<any> {
     return this.axios.get('/certificates', { params }).then((resp) => resp.data);
   }
+
   public async get(id: string): Promise<any> {
     return this.axios.get(`/certificates/${id}`).then((resp) => resp.data);
+  }
+
+  public async delete(id: string): Promise<any> {
+    return this.axios.delete(`/certificates/${id}`).then((resp) => resp.data);
+  }
+
+  public async create(certificateType: CertificateType, csrContent: string): Promise<any> {
+    return this.axios
+      .post('/certificates', {
+        data: {
+          type: 'certificates',
+          attributes: {
+            certificateType: certificateType,
+            csrContent: csrContent
+          }
+        }
+      })
+      .then((resp) => resp.data);
   }
 }
