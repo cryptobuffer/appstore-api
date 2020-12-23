e5ffedf5c423022720349f8b5f373fa55e808511
cryptobuffer <cryptobuffer@core.local>
2020-12-23 12:20:46 +0800

    test: add testcase of create and delete certificate

---
+++ tests/certificates/delete.ts
@@ -0,0 +1,14 @@
+import CertificatesAPI from '../../src/apis/CertificatesAPI';
+import client from '../client';
+
+const run = async () => {
+  let api = client.api('certificates') as CertificatesAPI;
+  try {
+    let result = await api.delete('<cert_id>');
+    console.log(JSON.stringify(result, null, '  '));
+  } catch (ex) {
+    console.error(ex.message);
+  }
+};
+
+run();
